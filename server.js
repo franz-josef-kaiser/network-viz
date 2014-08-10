86e41db6744f78e015faabe2105bfc25dc1ae6ec
Kaiser Franz Josef <wecodemore@gmail.com>
2014-08-10 19:08:46 +0200

    deploy(server) Tighten and tiedid up

--- server.js
+++ server.js
@@ -5,27 +5,24 @@ var sys         = require( 'sys' ),
 	fs          = require( 'fs' ),
 	connect     = require( 'connect' ),
 	serveStatic = require( 'serve-static' ),
-	sep         = "-------------------------\n",
+	sep         = "----------------------------------------------\n",
 	port        = process.env.npm_package_config_port || 8080;
-
-// Annoy user
-if ( "undefined" === port ) {
-	sys.puts(
-		sep
-		+ "Please run…\n"
-		+ "\tnpm config set network-viz:port 8080"
-		+ "…on the command line to define a port for the local server.\n"
-		+ sep + "\n"
-	);
-}
 
 // Start server
 connect()
 	.use( serveStatic( __dirname + '/www' ) )
-	.listen( port );
+	.listen( port, function() {
+		"undefined" === port && sys.puts(
+			sep
+			+ " Please run…\n\n"
+			+ " npm config set network-viz:port 8080\n\n"
+			+ " …on the command line to (re)define a port.\n"
+			+ sep
+		);
 
-sys.puts(
-	"-----------------------------------------\n"
-	+ " Server Running on http://localhost:" + port + "\n"
-	+ "-----------------------------------------\n"
-);
+		sys.puts(
+			sep
+			+ " Server Running on… http://localhost:" + port + "\n"
+			+ sep
+		)
+	} );
